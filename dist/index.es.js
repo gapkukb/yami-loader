1da94947ee06e8d8e6cf081d7952ec35a10ad722
AklanJoo <gapkukb@gmail.com>
2023-12-16 12:31:05 +0800

    update

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,68 +1,68 @@
 var w = Object.defineProperty, _ = Object.defineProperties;
 var C = Object.getOwnPropertyDescriptors;
-var v = Object.getOwnPropertySymbols;
+var f = Object.getOwnPropertySymbols;
 var O = Object.prototype.hasOwnProperty, L = Object.prototype.propertyIsEnumerable;
-var f = (r, t, e) => t in r ? w(r, t, { enumerable: !0, configurable: !0, writable: !0, value: e }) : r[t] = e, u = (r, t) => {
+var v = (r, t, e) => t in r ? w(r, t, { enumerable: !0, configurable: !0, writable: !0, value: e }) : r[t] = e, u = (r, t) => {
   for (var e in t || (t = {}))
-    O.call(t, e) && f(r, e, t[e]);
-  if (v)
-    for (var e of v(t))
-      L.call(t, e) && f(r, e, t[e]);
+    O.call(t, e) && v(r, e, t[e]);
+  if (f)
+    for (var e of f(t))
+      L.call(t, e) && v(r, e, t[e]);
   return r;
 }, m = (r, t) => _(r, C(t));
-var E = (r, t, e) => new Promise((n, o) => {
+var E = (r, t, e) => new Promise((o, n) => {
   var a = (s) => {
     try {
       d(e.next(s));
     } catch (i) {
-      o(i);
+      n(i);
     }
   }, l = (s) => {
     try {
       d(e.throw(s));
     } catch (i) {
-      o(i);
+      n(i);
     }
-  }, d = (s) => s.done ? n(s.value) : Promise.resolve(s.value).then(a, l);
+  }, d = (s) => s.done ? o(s.value) : Promise.resolve(s.value).then(a, l);
   d((e = e.apply(r, t)).next());
 });
 var g = /* @__PURE__ */ ((r) => (r.LOAD = "load", r.ERROR = "error", r.COMPLETE = "complete", r))(g || {});
 const y = document.head || document.getElementsByTagName("head")[0] || document.body, p = "loaded";
-function S(r, t, e) {
-  let n = e.id ? document.getElementById(e.id) : null;
-  if (!n) {
-    const o = `${t}[${t === "link" ? "href" : "src"}="${r}"]`;
-    n = y.querySelector(o);
+function T(r, t, e) {
+  let o = e.id ? document.getElementById(e.id) : null;
+  if (!o) {
+    const n = `${t}[${t === "link" ? "href" : "src"}="${r}"]`;
+    o = y.querySelector(n);
   }
-  return n || (n = document.createElement(t), Object.assign(n, e), y.appendChild(n)), n;
+  return o || (o = document.createElement(t), Object.assign(o, e), y.appendChild(o)), o;
 }
-const T = {
+const $ = {
   LoaderEvent: g,
   load(r, t = {}, e = {}) {
-    const n = t.src || t.href;
-    let o = S(n, r, t);
-    if (o.getAttribute(p) === "1")
-      return Promise.resolve(o);
+    const o = t.src || t.href;
+    let n = T(o, r, t);
+    if (n.getAttribute(p) === "1")
+      return Promise.resolve(n);
     let a;
     return new Promise((l, d) => {
       function s() {
-        l(o);
+        l(n);
       }
       function i() {
-        d(`Failed to load ${n}`);
+        d(`Failed to load ${o}`);
       }
-      o.addEventListener("load", s, !1), o.addEventListener("error", i, !1), a = () => {
+      n.addEventListener("load", s, !1), n.addEventListener("error", i, !1), a = () => {
         var h;
-        o.removeEventListener("load", s, !1), o.removeEventListener("error", i, !1), document.dispatchEvent(new CustomEvent("complete", { detail: o })), (h = e.oncomplete) == null || h.call(e, o);
+        n.removeEventListener("load", s, !1), n.removeEventListener("error", i, !1), document.dispatchEvent(new CustomEvent("complete", { detail: n })), (h = e.oncomplete) == null || h.call(e, n);
       };
       const c = 15 * 1e3;
       setTimeout(() => d(`Time out of ${c}ms`), c);
     }).then((l) => {
       var d;
-      return o.setAttribute(p, "1"), document.dispatchEvent(new CustomEvent("load", { detail: o })), (d = e.onload) == null || d.call(e, o), l;
+      return n.setAttribute(p, "1"), document.dispatchEvent(new CustomEvent("load", { detail: n })), (d = e.onload) == null || d.call(e, n), l;
     }).catch((l) => {
       var d;
-      throw o.remove(), document.dispatchEvent(new CustomEvent("error", { detail: l })), (d = e.onerror) == null || d.call(e, l), new Error(l);
+      throw n.remove(), document.dispatchEvent(new CustomEvent("error", { detail: l })), (d = e.onerror) == null || d.call(e, l), new Error(l);
     }).finally(a);
   },
   loadScript(r, t, e) {
@@ -92,22 +92,17 @@ const T = {
   },
   jsonp(r, t) {
     return E(this, null, function* () {
-      const [e, n] = r.split("?"), o = new URLSearchParams(n);
-      o.append(t.name || "jsonCallback", t.callee);
+      const [e, o] = r.split("?"), n = new URLSearchParams(o);
+      n.append(t.name || "jsonCallback", t.callee);
       const a = Object.create(t.callbacks || {}), l = a.onload;
       delete a.onload;
-      const d = yield this.loadScript(e + "?" + o.toString(), void 0, a), s = globalThis[t.callee]();
+      const d = yield this.loadScript(e + "?" + n.toString(), void 0, a), s = globalThis[t.callee]();
       return globalThis[t.callee] = null, d.remove(), l == null || l.call(t, s), s;
     });
   }
 };
-T.loadScript("demo.js", void 0, {
-  onload(r) {
-    console.log(this), console.log(r);
-  }
-});
 export {
   g as LoaderEvent,
-  T as default,
-  T as yamiLoader
+  $ as default,
+  $ as yamiLoader
 };
